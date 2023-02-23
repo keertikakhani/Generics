adfcdbecd7d41de14d9141186a883afc3267aef8
Your Name <you@example.com>
2023-02-23 13:42:16 +0530

    Finding maximum integer number among three integer numbers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,7 +6,16 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Generics");
+            Console.WriteLine("Enter first integer number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third integer number");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            int value = MaximumInteger.MaximumIntegerNumber(first, second, third);
+            Console.WriteLine("{0} is largest ", value);
+            Console.WriteLine("****************************");
 
             Console.ReadLine();
         }
