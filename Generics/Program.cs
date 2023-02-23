7e022540e2ab4faa199853af380941fd49f48bb9
Your Name <you@example.com>
2023-02-23 14:09:53 +0530

    Finding Maximum Floating point number among three numbers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,18 +6,38 @@ namespace Generics
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter first integer number");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second integer number");
-            int second = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter third integer number");
-            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose option from below");
+            Console.WriteLine("1.FindMaximumInteger\n2.FindMaximumFloat");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter first integer number");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second integer number");
+                    int second = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter third integer number");
+                    int third = Convert.ToInt32(Console.ReadLine());
 
-            int value = MaximumInteger.MaximumIntegerNumber(first, second, third);
-            Console.WriteLine("{0} is largest ", value);
-            Console.WriteLine("****************************");
+                    int value = MaximumInteger.MaximumIntegerNumber(first, second, third);
+                    Console.WriteLine("{0} is largest ", value);
+                    Console.WriteLine("****************************");
+                    break;
 
-            Console.ReadLine();
+                case 2:
+                    Console.WriteLine("Enter first floating point number");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter second floating point number");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter third floating point number");
+                    double num3 = Convert.ToDouble(Console.ReadLine());
+
+                    double value1 = MaximumFloat.MaximumFloatNumber(num1, num2, num3);
+                    Console.WriteLine("{0} is largest ", value1);
+                    Console.WriteLine("****************************");
+                    break;
+            }
+               Console.ReadLine();
         }
     }
 }
